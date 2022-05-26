89817900a6be9dc10f535142ea83c9deaa8f019f
Devansh Singh <56143122+blaady-baldy@users.noreply.github.com>
2022-05-26 12:12:18 +0530

    Update SimpleStore.sol

--- SimpleStore.sol
+++ SimpleStore.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.0;
 
 contract SimpleStore{
@@ -10,7 +12,10 @@ contract SimpleStore{
         string name;
     }
 
-    People public person = People({ num:7, name:"Devansh"});
+    People[] public people;                                                    //Declaring Array
+    mapping( string => uint256) public nameToFavNum;                           //Declaring Mapping - Dictionary   
+
+    // People public person = People({ num:7, name:"Devansh"});
 
     function store(uint256 _favNum) public{
         favNum = _favNum;
@@ -18,5 +23,10 @@ contract SimpleStore{
 
     function retrieve() public view returns(uint256){
         return favNum;
+    }
+
+    function addPerson(string memory _name, uint256 _favNum) public{
+        people.push(People(_favNum, _name));
+        nameToFavNum[_name] = _favNum;
     }
 }
